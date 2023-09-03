e59c6626e481a2674b44febe8d9415e7b1226a72
uladzislaupapou <vladcrwrpopov@gmail.com>
2023-09-03 12:50:00 +0200

    video popup

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,12 @@ function styles() {
 }
 
 function scripts() {
-	return src(['node_modules/jquery/dist/jquery.js', 'app/js/main.js'])
+	return src([
+		'node_modules/jquery/dist/jquery.js',
+		'node_modules/slick-carousel/slick/slick.js',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+		'app/js/main.js',
+	])
 		.pipe(concat('main.min.js'))
 		.pipe(uglify())
 		.pipe(dest('app/js'))
